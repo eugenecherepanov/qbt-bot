44b30466889fdedf2e8b48dbdaf8fac89a7f2ae6
eugenecherepanov <79087097+eugenecherepanov@users.noreply.github.com>
2024-02-26 10:01:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ from os import getenv
 # Qbittorrent settings
 QBITTORRENT_IP = "192.168.0.2"
 QBITTORRENT_PORT = "8089"
-QBITTORRENT_USER = "cherepanov"
-QBITTORRENT_PSW = "Jvcr789456123!"
+QBITTORRENT_USER = ""
+QBITTORRENT_PSW = ""
 
 # Bot access
-TG_TOKEN = "2004290040:AAH4LqQnkaPERoAdsGnq17vLG9wbJW4i5II"
-API_ID = "8741017"
-API_HASH = "e68a9ee595f16f7c79a69b961ba24499"
+TG_TOKEN = "<>"
+API_ID = ""
+API_HASH = ""
 
 # Bot settings
 NOTIFY = True
